7e570a48eda56b83e5f54a5ad910b7fe8530ac9a
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-23 16:18:21 +0100

    feat: add register page

---
+++ src/app/users/register/page.tsx
@@ -0,0 +1,116 @@
+"use client"
+import * as React from 'react';
+import { Box, Button, FormLabel, FormControl, Link, TextField, Typography, Stack } from '@mui/material';
+import { Card, SignUpContainer } from './styled';
+
+export default function SignUp() {
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      name: data.get('name'),
+      lastName: data.get('lastName'),
+      email: data.get('email'),
+      username: data.get('username'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <>
+      <SignUpContainer direction="column" justifyContent="space-between">
+        <Stack
+          sx={{
+            justifyContent: 'center',
+            height: '100dvh',
+            p: 2,
+          }}
+        >
+          <Card variant="outlined">
+            <Typography
+              component="h1"
+              variant="h4"
+              sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+            >
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+            >
+              <FormControl>
+                <FormLabel htmlFor="name">Full name</FormLabel>
+                <TextField
+                  autoComplete="name"
+                  name="name"
+                  required
+                  fullWidth
+                  id="name"
+                  placeholder="Jon Snow"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="username">Username</FormLabel>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  placeholder="CR7"
+                  name="username"
+                  autoComplete="username"
+                  variant="outlined"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  placeholder="your@email.com"
+                  name="email"
+                  autoComplete="email"
+                  variant="outlined"
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  placeholder="••••••"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  variant="outlined"
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+              >
+                Sign up
+              </Button>
+              <Typography sx={{ textAlign: 'center' }}>
+                Already have an account?
+                <span>
+                  <Link
+                    href="/session/login"
+                    variant="body2"
+                    sx={{ alignSelf: 'center' }}
+                  >
+                    Sign in
+                  </Link>
+                </span>
+              </Typography>
+            </Box>
+          </Card>
+        </Stack>
+      </SignUpContainer>
+    </>
+  );
+}

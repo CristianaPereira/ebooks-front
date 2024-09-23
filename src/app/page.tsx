365cde627cb2df4f0c2f585fd70a65180d6c5983
cristiana.pereira <c.pereira@runtime-revolution.com>
2024-09-23 11:58:45 +0100

    chore: add material ui + roboto font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Button variant="contained">Hello world</Button>
       <main className={styles.main}>
         <Image
           className={styles.logo}
